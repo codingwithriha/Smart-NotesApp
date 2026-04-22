a794600f24ad6f2d7d618fe677f427a8e62438e5
Laiba Ijaz <laibaijaz904@gmail.com>
2026-04-22 19:37:53 +0500

    add migration files in user table

--- database/migrations/2026_04_20_152051_create_users_table.php
+++ database/migrations/2026_04_20_152051_create_users_table.php
@@ -1,25 +1,25 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->boolean('is_admin')->default(false);   // true = admin, false = normal user
+            $table->boolean('is_blocked')->default(false); // admin can block users
+            $table->enum('theme', ['light', 'dark'])->default('light'); // user UI preference
+            $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
